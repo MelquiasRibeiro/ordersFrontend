32f7612e88be847537c91538fe881356d35ac9c4
Melquias Ribeiro <melquias10.mr@gmail.com>
2022-02-19 18:23:15 -0300

    feat: add order screens

--- src/pages/newOrder/index.tsx
+++ src/pages/newOrder/index.tsx
@@ -1,9 +1,206 @@
-import React from 'react';
+import React,{useState,FormEvent,ChangeEvent} from "react";
+import { FaSpinner,FaArrowLeft } from "react-icons/fa";
+import {Link,} from "react-router-dom";
+import { FiTrash2 } from "react-icons/fi";
+import { v4 as uuid } from "uuid";
 
-import { Container } from './styles';
+import {
+  Wrapper,
+  Content,
+  Input,
+  InfoContainer,
+  Button,
+  NewProduct,
+  ItemsContainer,
+  ItemRow,
+  DeleteButtom
+
+} from "./styles";
+import api from "../../services/api";
+import notify from "../../utils/notify";
+import { IItems } from "../dashboard";
 
 const NewOrder: React.FC = () => {
-  return( <Container ><p>NewOrder</p></Container>)
+
+  const [loading, setLoading] = useState(false);
+  const [userData,setUserData] =useState({
+    "customer_name": "",
+    "customer_email": "",
+    "customer_address": "",
+    })
+    const [items,setItems] = useState<IItems[]|[]>([])
+
+    const [unsavedItems,setUnsavedItems] = useState({
+        id:uuid(),
+				"product_name": "",
+				"product_price": 0,
+				"product_quantity": 0,
+		
+			})
+
+    function handleInputChange(e:ChangeEvent<HTMLInputElement>){
+      const {name,value} = e.target;
+      setUserData({...userData,[name]:value})
+
+  }
+  function addNewItem(){
+      setItems([...items,{...unsavedItems}
+    ])    
+    setUnsavedItems({
+        id:uuid(),
+				"product_name": "",
+				"product_price": 0,
+				"product_quantity": 0,
+		
+			})
+  }
+
+  function RemoveItem(id:string|undefined){
+    console.log(id)
+    const UpdatedItems= items.filter(item=>{
+      return item.id !==id
+    }
+    )
+   setItems(UpdatedItems)
+  }
+  
+  function setItemValue(
+    value: string,
+    name:string
+  ) {
+    setUnsavedItems({...unsavedItems,[name]:value})
+  }
+
+
+ async function HandleCreate(e:FormEvent) {
+  setLoading(true)
+
+   e.preventDefault()
+    try {
+      const data ={
+        ...userData,
+        items
+      }
+    await api.post('checkout',data);
+    notify('cadastrado com sucesso','sucess',)
+  } catch (error) {
+      notify("error ao salvar",'error')
+      
+
+  }finally{
+    setLoading(false)
+  }
+  
+  }
+
+  return( 
+    <Wrapper >
+      <Link to="/">
+        <FaArrowLeft color="#191920" size={24}/>
+      </Link>
+        <h1>Crie um novo registro</h1>
+        <Content onSubmit={HandleCreate} >
+              <InfoContainer>
+                <p>Nome</p>
+                <Input
+                  type="text"
+                  placeholder="Digite seu nome"
+                  required
+                  name="customer_name"
+                  id="customer_name"
+                  onChange={handleInputChange}
+
+                  />
+                <p>Email</p>
+                <Input
+                  type="email"
+                  placeholder="Digite seu email"
+                  required
+                  name="customer_email"
+                  id="customer_email"
+                  onChange={handleInputChange}
+                  />
+                <p>endereco</p>
+                <Input
+                  type="text"
+                  placeholder="Digite seu endereco"
+                  required
+                  name="customer_address"
+                  id="customer_address"
+                  onChange={handleInputChange}
+                  />
+
+              <ItemsContainer>
+              <h1>Produtos:</h1>
+              {
+                  items?.map((item)=>(
+                    
+                    <ItemRow key={item.id}>
+                      {
+                        <>
+                          <DeleteButtom type="button" onClick={()=>RemoveItem(item.id)}>
+                            <FiTrash2/>
+                          </DeleteButtom>
+                          <p>{item.product_name}</p>
+                          <p>{item.product_quantity}</p>
+                          <p>{item.product_price}</p>
+                        </>  
+                    
+                      }
+                 
+                       </ItemRow>
+                       
+                  ))
+              }
+                <ItemRow>
+                <div>
+
+                     <p>Nome do produto</p>
+                     <Input
+                       type="text"
+                       placeholder="Digite o nome do produto"
+                       name="product_name"
+                       id="customer_name"
+                       onChange={(e)=> setItemValue(e.target.value,"product_name") }
+                        value={unsavedItems.product_name}
+                       />
+                        </div>
+                        <div>
+     
+                       <p>Quantidade do produto</p>
+                          <Input
+                       type="number"
+                       placeholder="Digite a quantidade do produto"
+                       name="product_quantity"
+                       id="customer_name"
+                       onChange={(e)=> setItemValue(e.target.value,"product_quantity") }
+                       value={unsavedItems.product_quantity}
+                       />
+                     </div>
+                     <div>
+                     <p>Valor do produto</p>
+     
+                          <Input
+                       type="number"
+                       placeholder="Digite o valor do produto"
+                       name="product_price"
+                       id="customer_name"
+                       onChange={(e)=> setItemValue(e.target.value,"product_price") }
+                       value={unsavedItems.product_price}
+                       />
+                       </div>
+                       </ItemRow>
+                  <NewProduct type="button" onClick={addNewItem}>
+                     Adicionar produto
+                  </NewProduct>
+              </ItemsContainer>
+              </InfoContainer>
+              <Button >
+                {loading ? <FaSpinner color="#fff" size={14} /> : "SALVAR"}
+              </Button>
+        </Content>
+    </Wrapper>
+    )
 }
 
 export default NewOrder;
--- src/pages/newOrder/styles.ts
+++ src/pages/newOrder/styles.ts
@@ -1,5 +1,142 @@
-import styled from 'styled-components';
+import styled, { keyframes, css } from "styled-components";
 
-export const Container = styled.div`
+export const Wrapper = styled.div`
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin: 32px;
+  width: 90%;
+  background-color:#ffffff;
+  padding: 16px;
+  flex-direction: column;
+  border-radius: 12px;
+  > h1 {
+    margin-bottom: 32px;
+    color: #191920;
+    @media (max-width: 768px) {
+      text-align:center;
+      margin-top:16px;
+    }
+  }
+  >a{
+    align-self:flex-start;
+    margin-left:16px;
+    transition: opacity 0.2s;
+  &:hover {
+    opacity: 0.7;
+  }
+  }
+`;
+export const Content = styled.form`
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+  justify-content:center;
+  width: 100%;
+
+`
+
+export const Input = styled.input`
+  height: 40px;
+  border-radius: 8px;
+  padding: 8px 24px;
+  color: #191920;
+  border: solid 1px #191920;
+  margin-top: 16px;
+  font-size: 16px;
+  width: 100%;
   
+`;
+
+export const InfoContainer = styled.div`
+  width: 100%;
+
+  @media (max-width: 768px) {
+    margin:0 0 0 0;
+    }
+> p {
+    display: flex;
+    align-items: center;
+    margin: 16px 0 0px 0;
+    color: #191920;
+    font-size: 14px;
+    @media (max-width: 768px) {
+    margin:16 0 0 0;
+    }
+  }
+`;
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to{
+    transform: rotate(360deg)
+  }
+`;
+export const Button = styled.button`
+  display:flex;
+  align-items:center;
+  justify-content:center;
+  justify-self:center;
+  border-radius: 8px;
+  font-weight: bold;
+  height: 40px;
+  width: 100%;
+  align-self:flex-end;
+  justify-self:center;
+  border-style: none;
+  background-color: #191920;
+  margin-top: 32px;
+  transition: opacity 0.2s;
+  &:hover {
+    opacity: 0.7;
+  }
+  
+`;
+
+export const ItemsContainer = styled.div`
+>h1 {
+  color:#191920;
+  font-size: 16px;
+  margin: 16px 0;
+
+}
+p {
+  color:#191920;
+  margin: 16px 0 0 0;
+}
+`
+
+
+export const NewProduct = styled.button`
+ display: flex;
+ background-color:#191920 ;
+ padding: 4px;
+ border-radius: 4px;
+ border: none;
+ margin: 16px;
+
+ float:right;
+
+`;
+
+export const ItemRow = styled.div`
+  display: flex;
+  flex-direction:  row;
+  align-items: center;
+  justify-content: space-between;
+
+`
+
+
+export const DeleteButtom = styled.button`
+padding: 4px;
+display: flex;
+background-color:#191920 ;
+border: none;
+border-radius: 4px;
+
 `;
